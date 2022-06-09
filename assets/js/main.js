d8ca10a0b75b3a8ee434d782c91635135c233613
olya29011995 <104586225+olya29011995@users.noreply.github.com>
2022-06-09 19:16:41 +0300

    z

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// document.querySelector('.theme').addEventListener('change', (event) => {
+// if (event.target.nodeName === 'INPUT') {
+//     console.log('asdas');
+//     document.documentElement.classList.remove('dark', 'light');
+//     document.documentElement.classList.add(event.target)
+// }
+// });
+
+// let header = document.querySelector(".header");
+
+// document.addEventListener("scroll", () => {
+//   if (document.documentElement.scrollTop > 1) {
+//     header.classList.add("header_has-bg");
+//   } else {
+//     header.classList.remove("header_has-bg");
+//   }
+// });
+
+
+
+
